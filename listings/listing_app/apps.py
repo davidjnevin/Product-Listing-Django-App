efa106d24222c102875f491e459cdaea314ef2e7
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-09-13 20:38:41 +0200

    Added Poetry, Django and Listings

---
+++ listings/listing_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ListingAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "listing_app"
---
+++ listings/listing_app/models.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from django.db import models
+from django.utils.timezone import now
+
+
+class ListingsApp(models.Model):
+    class SaleType(models.TextChoices):
+        PICK_UP = "Available for pickup"
+        SHIP = "Available for shipping"
+
+    class ConditionType(models.TextChoices):
+        USED = "Used"
+        NEW = "New"
+
+    class ProductType(models.TextChoices):
+        BIKE = "Bike"
+        PARTS = "Parts"
+        MODELS = "Models"
+        OTHER = "Other"
+
+    title = models.CharField(max_length=100)
+    condition = models.CharField(
+        max_length=50, choices=ConditionType.choices, default=ConditionType.USED
+    )
+    product_type = models.CharField(
+        max_length=50, choices=ProductType.choices, default=ProductType.BIKE
+    )
+    sale_type = models.CharField(
+        max_length=50, choices=SaleType.choices, default=SaleType.SHIP
+    )
+    price = models.FloatField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=25)
+    zipcode = models.CharField(max_length=25)
+    main_photo = models.ImageField()
+    photo_1 = models.ImageField()
+    photo_2 = models.ImageField(blank=True)
+    list_date = models.DateTimeField(default=now)
+    contact_email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Listings"
