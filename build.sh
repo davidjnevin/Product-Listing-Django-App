04d2fb59fc1574d748f0df7f1a29a3d6a1319e3f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-09-20 18:26:13 +0200

    Added listings dir to manage.p calls

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 poetry install
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python listings/manage.py collectstatic --no-input
+python listings/manage.py migrate
