42dd72d9e188dda62adde88227901ff52a3fc54c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-09-20 13:52:29 +0200

    Updated Settings and Upgraded Poetry

--- listings/listings/settings.py
+++ listings/listings/settings.py
@@ -145,7 +145,15 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # STATIC_ROOT = os.path(BASE_DIR, 'static')
 
